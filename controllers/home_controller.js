8b55f078126a37c0c9acaeab079cde4c8ee3234c
Abheetarora <62665287+Abheetarora@users.noreply.github.com>
2021-03-01 00:12:05 +0530

    add views ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 module.exports.home= function(req, res){
-    return res.end('<h1>Express is up for codeial</h1>')
+    return res.render('home', {
+        title: "tenu le kr jaana apne naal ahaa ahaaa"
+    });
 }
 //module.export.actionName=function(req,res){}
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,5 +2,7 @@ module.exports.profile=function(req,res){
     return res.end('<h1>User profile</h1>');
 }
 module.exports.profound=function(req,res){
-    return res.end('<h1>this is user ${2+3}</h1>')
+    return res.render('users',{
+        title:"User"
+    });
 }
