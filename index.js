b2e6094c5c842093e2d80888f8441bcb82f45d84
Abheetarora <62665287+Abheetarora@users.noreply.github.com>
2021-02-28 23:45:58 +0530

    set view engine

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const port=8000;
 
 //use exports router
 app.use('/',require('./routes'))
-
-
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err)
