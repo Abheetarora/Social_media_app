8428d119c6a999717016cb6e55e77b66a4372c41
Abheetarora <62665287+Abheetarora@users.noreply.github.com>
2021-03-19 00:49:32 +0530

    posts show

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,19 @@
+const Post =require('../models/post');
+
 module.exports.home= function(req, res){
-   
+//    Post.find({},function(err, posts){
+//     return res.render('home', {
+//         title: "Quora Clone | Home",
+//         posts:posts
+//     });
+// });
+
+//populate the user of each of each post 
+Post.find({}).populate('user').exec(function(err,posts){
     return res.render('home', {
-        title: "HOME"
+        title: "Quora Clone | Home",
+        posts:posts
     });
+});
 }
 //module.export.actionName=function(req,res){}
--- routes/posts.js
+++ routes/posts.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const postsController = require('../controllers/posts_controller')
-router.get('/',postsController.posts)
+router.post('/create',postsController.create)
+
 module.exports=router;
