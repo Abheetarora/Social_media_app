597facf20e0ad3786c3be72c68dc86d7bd8f2365
Abheetarora <62665287+Abheetarora@users.noreply.github.com>
2021-03-21 00:41:10 +0530

    update function and profiles

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,5 @@
 const Post =require('../models/post');
+const User=require('../models/user');
 
 module.exports.home= function(req, res){
 //    Post.find({},function(err, posts){
@@ -18,10 +19,16 @@ Post.find({})
     }
 })
 .exec(function(err,posts){
+
+User.find({},function(err,users){
     return res.render('home', {
         title: "Quora Clone | Home",
-        posts:posts
+        posts:posts,
+        all_users:users
     });
 });
+
+    
+})
 }
 //module.export.actionName=function(req,res){}
