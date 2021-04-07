9849b9fbb14b4d3bd7e9478c737b699399c472ae
Abheetarora <62665287+Abheetarora@users.noreply.github.com>
2021-04-07 19:03:26 +0530

    jwt

--- models/user.js
+++ models/user.js
@@ -1,22 +1,47 @@
 const mongoose = require('mongoose');
 
-const UserSchema= mongoose.Schema({
-    email:{
-        type:String,
+const multer = require('multer');
+const path = require('path');
+const AVATAR_PATH = path.join('/uploads/users/avatars');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
         required: true,
-        unique:true
+        unique: true
     },
-    password:{
-        type:String,
+    password: {
+        type: String,
         required: true
     },
-    name:{
-        type:String,
+    name: {
+        type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
-},
-{
-    timestamp:true,
+}, {
+    timestamps: true
 });
-const User=mongoose.model('User',UserSchema);
+
+
+let storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+
+// static
+userSchema.statics.uploadedAvatar = multer({storage:  storage}).single('avatar');
+userSchema.statics.avatarPath = AVATAR_PATH;
+
+
+
+const User = mongoose.model('User', userSchema);
+
 module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
-const express=require('express');
-const router=express.Router();
+const express = require('express');
 
-console.log("router loaded");
-const homeController=require('../controllers/home_controller');
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+
+console.log('router loaded');
+
+
 router.get('/', homeController.home);
-router.use('/users',require('./users'));
-router.use('/posts',require('./posts'));
-router.use('/posts',require('./posts'));
-router.use('/comments',require('./comments'));
-//for any further routes,access from here
-//router.use('/routername',require('./routerfile'))
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
+
+
+router.use('/api', require('./api'));
+
+// for any further routes, access from here
+// router.use('/routerName', require('./routerfile));
+
 
 module.exports = router;
