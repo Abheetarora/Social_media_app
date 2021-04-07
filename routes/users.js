7440a334c244329e172ae7d7660f4b8c02c390ed
Abheetarora <62665287+Abheetarora@users.noreply.github.com>
2021-04-07 19:59:40 +0530

    half auth

--- routes/users.js
+++ routes/users.js
@@ -14,9 +14,14 @@ router.get('/sign-in',usersController.signin)
 router.post('/create',usersController.create)
 router.get('/sign-out',usersController.destroySession)
 //use passport as a midleware to authenticate
-// use passport as a middleware to authenticate
 router.post('/create-session', passport.authenticate(
     'local',
     {failureRedirect: '/users/sign-in'},
 ), usersController.createSession);
+
+router.get('/auth/google',passport.authenticate('google',{scope:['email']}));
+router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/sign-in'}),usersController.createSession);             
+
+
+
 module.exports=router;
